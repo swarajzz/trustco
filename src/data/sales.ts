852ade1fc17066d30aeca7e29da100caf19add59
swarajzz <sharma.swaraj0403@gmail.com>
2025-03-13 12:08:35 +0530

    added sales video thumbnail

--- src/data/sales.ts
+++ src/data/sales.ts
@@ -3,5 +3,5 @@ export const salesData = {
   highlightText: "Teams & Professionals",
   description:
     "Circuit is flexible and affordable and offers you exceptional support to achieve your career goals. Circuit is a Global training provider based across the UK that specialises.",
-  videoThumbnail: "/placeholder.svg?height=600&width=800",
+  videoThumbnail: "/images/sales/Video.png",
 };
